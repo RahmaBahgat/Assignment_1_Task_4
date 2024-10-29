f3a56c221163db3949c060217c18207cd046d9a3
RahmaBahgat <162814707+RahmaBahgat@users.noreply.github.com>
2024-10-29 21:50:54 +0300

    Update CU.h

--- CU.h
+++ CU.h
@@ -4,15 +4,17 @@
 #include "Register.h"
 #include "Memory.h"
 #include "ALU.h"
+#include <string>
 
 class CU {
 public:
-    void load(int idxReg, int intMem, Register& reg, Memory& mem);
-    void load(int idxReg, std::string val, Register& reg);
-    void store(int idxReg, int idxMem, Register& reg, Memory& mem);
-    void move(int idx1Reg, int idx2Reg, Register& reg);
-    void jump(int idxReg, int idxMem, Register& reg, int& PC);
-    void halt() const;
+    void load(int idxReg, int intMem, Register& reg, Memory& mem);         // RXY
+    void load(int idxReg, std::string val, Register& reg);                 // RXY (register with bit pattern)
+    void store(int idxReg, int idxMem, Register& reg, Memory& mem);        // RXY (store)
+    void move(int idx1Reg, int idx2Reg, Register& reg);                    // 0RS
+    void jump(int idxReg, int idxMem, Register& reg, int& PC);             // BXY
+    void halt() const;                                                     // C000
 };
 
 #endif
+
